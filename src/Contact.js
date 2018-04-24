ed631d49e3747ef0b6f94f31b92f451fee1a063c
Jefferson Griebel <jeffersongriebel@Jeffersons-MacBook-Pro-2.local>
2018-04-23 20:07:46 -0400

    finished css for the projects component

--- src/Contact.js
+++ src/Contact.js
@@ -9,9 +9,9 @@ class Contact extends Component {
                     <h1 className='contact-txt'>Contact Information</h1>
                 </header>
                 <ul className='li-txt'>
-                    <li>Jefferson Griebel</li>
-                    <li><a href="mailto:jefferson.griebel@gmail.com">jefferson.griebel@gmail.com</a></li>
-                    <li>352-314-4762</li>
+                    <li className='cont-li'>Jefferson Griebel</li>
+                    <li className='cont-li'><a href="mailto:jefferson.griebel@gmail.com">jefferson.griebel@gmail.com</a></li>
+                    <li className='cont-li'>352-314-4762</li>
                 </ul>
             </div>
         )
--- src/Projects.js
+++ src/Projects.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-
+import './projects.css';
 class Projects extends Component{
     render(){
         return(
-            <div>
-                <h2>Projects</h2>
-                    <ul>
-                        <li>
+            <div className='txt'>
+                <h2 className='pro-txt-h2'>Projects</h2>
+                    <ul className='pro-li-txt'>
+                        <li className='txt-li-style'>
                             <a href="https://jgweather-app.herokuapp.com/">React weather app</a>
                         </li>
-                        <li>
+                        <li className='txt-li-style'>
                             <a href="https://mtg-deckbuilder.herokuapp.com/">Magic the Gathering Deck Builder</a>
                         </li>
                     </ul>
