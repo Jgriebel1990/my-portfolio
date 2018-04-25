9609214e1ada1d3353568936e5a7bab1e1acf3b5
Jefferson Griebel <jeffersongriebel@Jeffersons-MacBook-Pro-2.local>
2018-04-25 00:27:10 -0400

    updated the about me component

--- src/Contact.js
+++ src/Contact.js
@@ -12,6 +12,9 @@ class Contact extends Component {
                     <li className='cont-li'>Jefferson Griebel</li>
                     <li className='cont-li'><a href="mailto:jefferson.griebel@gmail.com">jefferson.griebel@gmail.com</a></li>
                     <li className='cont-li'>352-314-4762</li>
+                    <li className='cont-li'>
+                        <a href="https://github.com/Jgriebel1990">Github profile</a>
+                    </li>
                 </ul>
             </div>
         )
