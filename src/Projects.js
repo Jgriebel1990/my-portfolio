d44db4704519e314ff3605ce0cb12795713fceed
Jefferson Griebel <jeffersongriebel@Jeffersons-MacBook-Pro-2.local>
2018-05-04 16:21:27 -0400

    added another project to my portfolio

--- src/Projects.js
+++ src/Projects.js
@@ -12,6 +12,9 @@ class Projects extends Component{
                         <li className='txt-li-style'>
                             <a href="https://mtg-deckbuilder.herokuapp.com/">Magic the Gathering Deck Builder</a>
                         </li>
+                        <li className='txt-li-style'>
+                            <a href="https://jgriebel1990.github.io/number-guess/">The first app I ever made using Javascript</a>
+                        </li>
                     </ul>
             </div>
         )
