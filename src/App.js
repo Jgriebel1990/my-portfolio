dda7e0967ea378cbb738bdbd8f633327dcd70004
Jefferson Griebel <jeffersongriebel@Jeffersons-MacBook-Pro-2.local>
2018-05-08 12:17:33 -0400

    deleted about me component and css

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import './styles/App.css';
 import Contact from './Contact';
 import Projects from './Projects';
 import Navbar from './Navbar';
-import AboutMe from './AboutMe';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Route path='/aboutme' exact component={AboutMe} />
         <Route path='/contact' exact component={Contact} />
         <Route path='/projects' exact component={Projects} />
       </div>
